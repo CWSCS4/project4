5369872159b32d2058150b81b0685734af6672ca
joshbernardo <jbernardo@commschool.org>
2017-10-18 12:35:15 -0400

    Update Fill.asm

--- fill/Fill.asm
+++ fill/Fill.asm
@@ -11,4 +11,48 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(START)
+@SCREEN
+D=A
+@0      //SET PIXEL NUMBER
+M=D
+
+(KBDCHECK)
+@KBD
+D=M
+@WHITE
+D;JEQ
+@BLACK
+D;JGT
+@KBDCHECK
+0;JMP
+
+(WHITE)
+@1      //TRACK FILLER COLOR
+M=0
+@SHIFT
+0;JMP
+
+(BLACK)
+@1      //TRACK FILLER COLOR
+M=-1
+@COLOR
+0;JMP
+
+(COLOR)
+@1      //CHECK FILLER COLOR
+D=M
+@0      //FILL PIXEL WITH COLOR
+A=M
+M=D
+@0      //INC PIXEL NUMBER
+M=M+1
+D=M
+@SCREEN
+D=A-D
+@0      //INC PIXEL NUMBER
+A=M+1
+@COLOR  //LOOP THROUGH COLOR IF 
+D;JGT
+@START  //GO BACK TO START IF DONE
+0;JMP
