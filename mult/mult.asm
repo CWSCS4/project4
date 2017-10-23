94e24fa08ae199210e12ec8e0958b2475f4eca5b
joshbernardo <jbernardo@commschool.org>
2017-10-23 09:53:37 -0400

    Update mult.asm

--- mult/mult.asm
+++ mult/mult.asm
@@ -7,27 +7,28 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 @sum    
-M=0     // Initialize sum to 0
+M=0     //Make sum = 0
 @R1
 D=M
 @count  
-M=D     // Initialize count to the value at RAM[1]
+M=D     //Store RAM[1] in count
 
 (LOOP)  
 @count
 D=M
-@END        D;JEQ   // Check if count is 0, if so, goto END
+@END
+D;JEQ   //If we've added RAM[0] RAM[1] times -> END
 @R0
 D=M
 @sum
-M=M+D   // Add RAM[0] to sum
+M=M+D   //Add RAM[0] to sum
 @count
-M=M-1   // Decrement count by 1
+M=M-1   //Now there is count - 1 additions to go
 @LOOP
-0;JMP   // Loop again
+0;JMP   //Loop again
 
 (END)   
 @sum
 D=M
 @R2
-M=D     // Write sum to RAM[2]
+M=D     //Write sum to RAM[2]
