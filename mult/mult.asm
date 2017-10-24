a78311b8c66b4a52205acf444c4863730aeb551c
Alex Lehner <alexxlehner@gmail.com>
2017-10-23 23:10:41 -0400

    Implemented & commented the three functions

--- mult/mult.asm
+++ mult/mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//Initialize RAM[2] to 0
+@2
+M=0
+
+(LOOP)
+
+//Decrements RAM[0], and ends the program if RAM[0] was 0 before it was decremented
+@0
+D=M-1
+M=D
+@END
+D;JLT
+
+//Adds RAM[1] to RAM[2]
+@1
+D=M
+@2
+M=M+D
+
+@LOOP
+0;JMP
+
+
+//By the end of the loop, RAM[1] will be added to RAM[2] exactly RAM[0] times, so RAM[2]=RAM[0]*RAM[1]
+(END)
+
+@END
+0;JMP
